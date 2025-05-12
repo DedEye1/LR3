73cf5825e13f76bd86718a0cbf7ae06879d6ff4a
DedEye1 <dedeye-wrk@yandex.ru>
2025-05-12 18:56:35 +0600

    LR4 - unit-tests changed

--- test.py
+++ test.py
@@ -1,27 +1,52 @@
 import unittest
-from app import app
+
+class app:
+    def calculate(self, val: float, _from: str, _to: str) -> float:
+        diction = {
+            "rur" : {
+                "rur": 1,
+                "usd": 0.0124,
+                "eur": 0.0109
+            },
+            "usd" : {
+                "rur": 80.9644,
+                "usd": 1,
+                "eur": 0.8808
+            },
+            "eur" : {
+                "rur": 91.9169,
+                "usd": 1.1353,
+                "eur": 1
+            }
+        }
+        return val * diction[_from][_to]
+
+
+
 
 class TestApp(unittest.TestCase):
     def setUp(self):
-        self.app = app.test_client()
+        self.app = app()
 
-    def test_form_post_valid_data(self):
-        test_cases = [
-            {"from": "rur", "to": "usd", "convertable": "100", "expected": 1.24},
-            {"from": "usd", "to": "eur", "convertable": "50", "expected": 44.04},
-            {"from": "eur", "to": "rur", "convertable": "10", "expected": 919.169},
-        ]
+    def test_rur_to_rur(self):
+        self.assertEqual(self.app.calculate(100, "rur", "rur"), 100)
 
-        for case in test_cases:
-            response = self.app.post(
-                '/',
-                data={
-                    'from': case['from'],
-                    'to': case['to'],
-                    'convertable': case['convertable']
-                }
-            )
-            self.assertIn(str(case['expected']), response.get_data(as_text=True))
+    def test_rur_to_usd(self):
+        self.assertEqual(self.app.calculate(100, "rur", "usd"), 1.24)
+
+    def test_rur_to_eur(self):
+        self.assertEqual(self.app.calculate(100, "rur", "eur"), 1.09)
+
+    def test_usd_to_rur(self):
+        self.assertEqual(self.app.calculate(100, "usd", "rur"), 8096.44)
+
+    def test_usd_to_usd(self):
+        self.assertEqual(self.app.calculate(100, "usd", "usd"), 100)
+
+    def test_usd_to_eur(self):
+        self.assertEqual(self.app.calculate(100, "usd", "eur"), 88.08)
+
+
 
 if __name__ == '__main__':
     unittest.main()
